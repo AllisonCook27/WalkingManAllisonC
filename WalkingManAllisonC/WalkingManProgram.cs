c2aa05c26d93eab1009bff568b3e7cee2279e8c5
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-03-22 11:53:47 -0400

    start

--- WalkingManAllisonC/WalkingManProgram.cs
+++ WalkingManAllisonC/WalkingManProgram.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace WalkingManAllisonC
 {
-    static class Program
+    static class WalkingManProgram
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace WalkingManAllisonC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WalkingManForm());
+            Application.Run(new frmWalkingMan());
         }
     }
 }
