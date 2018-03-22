f461881e27b63c4398de6cee02f5632ed62226d0
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-03-22 11:59:29 -0400

    done(almost)

--- WalkingManAllisonC/WalkingManForm.Designer.cs
+++ WalkingManAllisonC/WalkingManForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WalkingManAllisonC
 {
-    partial class Form1
+    partial class frmWalkingMan
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picMan = new System.Windows.Forms.PictureBox();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("ISOCPEUR", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(111, 41);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(75, 27);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::WalkingManAllisonC.Properties.Resources.walk1;
+            this.picMan.Location = new System.Drawing.Point(62, 92);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(178, 177);
+            this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMan.TabIndex = 1;
+            this.picMan.TabStop = false;
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(319, 24);
+            this.mnuMenu.TabIndex = 2;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(319, 309);
+            this.Controls.Add(this.picMan);
+            this.Controls.Add(this.btnWalk);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Allison C";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picMan;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- WalkingManAllisonC/WalkingManForm.cs
+++ WalkingManAllisonC/WalkingManForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 21 March, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Walking Man
+ * This program animates and man walking when a button is clicked
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +14,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WalkingManAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmWalkingMan : Form
     {
-        public Form1()
+        public frmWalkingMan()
         {
             InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program when clicked
+            this.Close();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            //variables and constants
+            const int MAX_FRAMES = 10;
+            int picCounter = 1;
+
+            //loop to animate the man walking
+            while (picCounter < MAX_FRAMES)
+            {
+                if (picCounter == 1)
+                {
+                    picMan.Image = Properties.Resources.walk1;
+                }
+                else if (picCounter == 2)
+                {
+                    picMan.Image = Properties.Resources.walk2;
+                }
+                else if (picCounter == 3)
+                {
+                    picMan.Image = Properties.Resources.walk3;
+                }
+                else if (picCounter == 4)
+                {
+                    picMan.Image = Properties.Resources.walk4;
+                }
+                else if (picCounter == 5)
+                {
+                    picMan.Image = Properties.Resources.walk5;
+                }
+                else if (picCounter == 6)
+                {
+                    picMan.Image = Properties.Resources.walk6;
+                }
+                else if (picCounter == 7)
+                {
+                    picMan.Image = Properties.Resources.walk7;
+                }
+                else if (picCounter == 8)
+                {
+                    picMan.Image = Properties.Resources.walk8;
+                }
+                else if (picCounter == 9)
+                {
+                    picMan.Image = Properties.Resources.walk9;
+                }
+                else if (picCounter == 10)
+                {
+                    picMan.Image = Properties.Resources.walk10;
+                }
+
+                //increment the counter
+                picCounter = picCounter + 1;
+
+                //refresh the form
+                this.Refresh();
+
+                //pause the loop for 100 miliseconds
+                Thread.Sleep(100);
+            }
         }
     }
 }
